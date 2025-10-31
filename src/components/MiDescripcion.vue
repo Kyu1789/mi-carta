a3d9cea156567f85154e077676af345537b98dc5
Kyu <samackermanxvi@gmail.com>
2025-10-30 22:56:13 -0500

    Initial commit

---
+++ src/components/MiDescripcion.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-card class="q-ma-md">
+    <q-card-section>
+      <div class="text-h6">Mi Carta de Presentación</div>
+    </q-card-section>
+
+    <q-card-section>
+      <p>
+        Soy un estudiante de diseño web apasionado por crear aplicaciones 
+        modernas con Vue.js y Quasar. Este es mi proyecto para el curso.
+      </p>
+      <p>
+        (Aquí puedes agregar más información sobre ti, tus habilidades, etc.)
+      </p>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+export default {
+  name: 'MiDescripcion'
+}
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page padding> <mi-descripcion />
+
+    </q-page>
+</template>
+
+<script>
+// 1. Importamos los componentes que creamos
+// (OJO: No importamos MiEncabezado porque lo pondremos en otro lado)
+import MiDescripcion from 'src/components/MiDescripcion.vue'
+
+export default {
+  name: 'IndexPage',
+
+  // 2. Los "registramos" para poder usarlos en el <template>
+  components: {
+    MiDescripcion
+  }
+}
+</script>
