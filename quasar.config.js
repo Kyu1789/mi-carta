2adeb92c0ff2332154d3fcf4c8dadf2e489c831d
Kyu <samackermanxvi@gmail.com>
2025-10-30 23:42:50 -0500

    Fix: Añadiendo _redirects para despliegue en Netlify

--- quasar.config.js
+++ quasar.config.js
@@ -32,8 +32,6 @@ export default defineConfig((/* ctx */) => {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#build
     build: {
-      publicPath: '',
-      vueRouterMode: 'hash',
       target: {
         browser: ['es2022', 'firefox115', 'chrome115', 'safari14'],
         node: 'node20',
